bd5c263965ef1b134f6d873b45547ef52de7d5cf
Tim You <timyou@users.noreply.github.com>
2022-04-02 20:17:40 +0800

    Delete dnsSwitch.sh

--- dnsSwitch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4
-
-sudo dscacheutil -flushcache
-sudo kill -HUP  mDNSResponder
-
-
-# log stream --predicate 'process == "mDNSResponder"' --info
